13e886246c139887fbdb42dd9f89227fdbeae21b
chhak0503 <chhak0503@gmail.com>
2023-04-05 16:51:57 +0900

    2023/04/05 수업예제

--- Ch03/src/App.vue
+++ Ch03/src/App.vue
@@ -9,9 +9,15 @@
 
   <h4>2)Options API</h4>
   <ApiComponent1 />
+  <ApiComponent2 />
+  <ApiComponent3 />
+  <ApiComponent4 />
   <hr />
 
   <h4>3)컴포넌트 통신</h4>
+  <UComponent1 :name="name" />
+  <UComponent1 :name="name" :age="age" />
+
   <hr />
 
   <h4>4)컴포넌트 생명주기</h4>
@@ -23,14 +29,28 @@ import Component1 from "./components/sub1/Component1.vue";
 import Component2 from "./components/sub1/Component2.vue";
 import Component3 from "./components/sub1/Component3.vue";
 import ApiComponent1 from "./components/sub2/ApiComponent1.vue";
+import ApiComponent2 from "./components/sub2/ApiComponent2.vue";
+import ApiComponent3 from "./components/sub2/ApiComponent3.vue";
+import ApiComponent4 from "./components/sub2/ApiComponent4.vue";
+import UComponent1 from "./components/sub3/UComponent1.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      name: "홍길동",
+      age: 21,
+    };
+  },
   components: {
     Component1,
     Component2,
     Component3,
     ApiComponent1,
+    ApiComponent2,
+    ApiComponent3,
+    ApiComponent4,
+    UComponent1,
   },
 };
 </script>
