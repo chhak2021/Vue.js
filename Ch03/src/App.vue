516bb59e1b867c310143a3409b042fdec6b251fb
chhak0503 <chhak0503@gmail.com>
2023-04-05 15:44:47 +0900

    2023/04/05 수업예제

---
+++ Ch03/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h3>Ch03.Component 실습</h3>
+
+  <h4>1)컴포넌트 기본</h4>
+  <Component1></Component1>
+  <Component2 />
+  <Component3 />
+  <hr />
+
+  <h4>2)Options API</h4>
+  <ApiComponent1 />
+  <hr />
+
+  <h4>3)컴포넌트 통신</h4>
+  <hr />
+
+  <h4>4)컴포넌트 생명주기</h4>
+  <hr />
+</template>
+
+<script>
+import Component1 from "./components/sub1/Component1.vue";
+import Component2 from "./components/sub1/Component2.vue";
+import Component3 from "./components/sub1/Component3.vue";
+import ApiComponent1 from "./components/sub2/ApiComponent1.vue";
+
+export default {
+  name: "App",
+  components: {
+    Component1,
+    Component2,
+    Component3,
+    ApiComponent1,
+  },
+};
+</script>
