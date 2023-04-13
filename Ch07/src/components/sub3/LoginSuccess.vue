cbd43a115e1e4a7af17c99fd6e5b8872c80d52e0
chhak0503 <chhak0503@gmail.com>
2023-04-13 17:19:12 +0900

    2023/04/13 수업예제

---
+++ Ch07/src/components/sub3/LoginSuccess.vue
@@ -0,0 +1,18 @@
+<template>
+  <h4>로그인 성공</h4>
+  <p>
+    XXX 님 반갑습니다.
+    <a href="#" @click.prevent="logout">로그아웃</a>
+  </p>
+</template>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const logout = () => {
+  localStorage.removeItem("accessToken");
+  router.push("/jwt/login");
+};
+</script>
+<style scoped></style>
