f023666408c49dacde488e6c879950e8b9e704d3
chhak0503 <chhak0503@gmail.com>
2023-04-26 16:25:08 +0900

    2023/04/25 수업예제

--- Voard/src/components/Write.vue
+++ Voard/src/components/Write.vue
@@ -7,11 +7,16 @@
     <v-main>
       <v-container>
         <v-sheet max-width="800" class="mx-auto">
-          <v-text-field label="제목입력" variant="outlined"></v-text-field>
+          <v-text-field
+            label="제목입력"
+            variant="outlined"
+            v-model="article.title"
+          ></v-text-field>
           <v-textarea
             label="내용입력"
             variant="outlined"
             rows="12"
+            v-model="article.content"
           ></v-textarea>
           <v-file-input label="파일첨부" variant="outlined"></v-file-input>
           <v-sheet class="text-right">
@@ -24,15 +29,40 @@
   </v-app>
 </template>
 <script setup>
+import { reactive, computed } from "vue";
 import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import axios from "axios";
 
 const router = useRouter();
+const store = useStore();
+
+const article = reactive({
+  title: null,
+  content: null,
+  uid: null,
+});
 
 const btnCancel = () => {
   router.push("/list");
 };
 const btnWrite = () => {
-  alert("등록완료");
+  //alert("등록완료");
+  const user = store.getters.user;
+  article.uid = user.uid;
+
+  console.log(article);
+
+  axios
+    .post("http://localhost:8080/Voard/write", article)
+    .then((response) => {
+      console.log(response);
+
+      //router.push("/user/login");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 </script>
 <style scoped></style>
--- Voard/src/components/user/Register.vue
+++ Voard/src/components/user/Register.vue
@@ -22,7 +22,30 @@
                   ></v-text-field>
                 </v-col>
                 <v-col cols="7">
-                  <v-btn color="warning" class="ml-2">중복확인</v-btn>
+                  <v-btn
+                    :loading="loading"
+                    color="warning"
+                    class="ml-2"
+                    @click="btnCheckUid"
+                    >중복확인</v-btn
+                  >
+                  <v-chip
+                    v-if="rsChip1"
+                    class="ma-2"
+                    color="red"
+                    text-color="white"
+                  >
+                    이미 사용중인 아이디 입니다.
+                  </v-chip>
+
+                  <v-chip
+                    v-if="rsChip2"
+                    class="ma-2"
+                    color="green"
+                    text-color="white"
+                  >
+                    사용 가능한 아이디 입니다.
+                  </v-chip>
                 </v-col>
               </v-row>
               <v-row no-gutters="true" class="mb-2">
@@ -161,6 +184,7 @@
 import { reactive } from "vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
+import { ref } from "vue";
 
 const router = useRouter();
 const user = reactive({
@@ -176,6 +200,38 @@ const user = reactive({
   addr2: null,
 });
 
+const rsChip1 = ref(false);
+const rsChip2 = ref(false);
+const loading = ref(false);
+
+const btnCheckUid = () => {
+  //console.log(user);
+  loading.value = true;
+
+  axios
+    .get("http://localhost:8080/Voard/user/checkUid", {
+      params: { uid: user.uid },
+    })
+    .then((response) => {
+      console.log(response);
+
+      setTimeout(() => {
+        loading.value = false;
+
+        const count = response.data;
+        if (count > 0) {
+          rsChip1.value = true;
+          rsChip2.value = false;
+        } else {
+          rsChip1.value = false;
+          rsChip2.value = true;
+        }
+      }, 600);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 const btnRegister = () => {
   //console.log(user);
   axios
